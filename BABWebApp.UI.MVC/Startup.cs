647e4778afc55cb4d6a62114ededc677611a399a
SpencerPearson <spencer.pearson.816@outlook.com>
2021-03-13 12:41:46 -0600

    added identity

--- BABWebApp.UI.MVC/Startup.cs
+++ BABWebApp.UI.MVC/Startup.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.Owin;
-using Owin;
+﻿using Owin;
 
-[assembly: OwinStartupAttribute(typeof(BABWebApp.UI.MVC.Startup))]
 namespace BABWebApp.UI.MVC
 {
     public partial class Startup
